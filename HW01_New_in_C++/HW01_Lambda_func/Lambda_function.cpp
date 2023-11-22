72af4300879bcb67a6bebed6d0bee9a59e7e4806
yenox <felixxx@vivaldi.net>
2023-11-22 18:07:27 +0300

    add HW

---
+++ HW01_New_in_C++/HW01_Lambda_func/Lambda_function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  auto big_int = 100'000'000;
+  std::cout << big_int << std::endl;
+return 0;
+}
---
+++ HW01_New_in_C++/HW01_Lambda_func/build/CMakeFiles/Lambda_function.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lambda_function.dir/Lambda_function.cpp.obj"
+  "CMakeFiles/Lambda_function.dir/Lambda_function.cpp.obj.d"
+  "Lambda_function.exe"
+  "Lambda_function.exe.manifest"
+  "Lambda_function.pdb"
+  "libLambda_function.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lambda_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
