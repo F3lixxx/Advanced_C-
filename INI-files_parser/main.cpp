793d64e0561feb3f9286b03063ea84a7f9e00051
yenox <felixxx@vivaldi.net>
2024-02-07 23:53:26 +0300

    change ini parser

--- INI-files_parser/main.cpp
+++ INI-files_parser/main.cpp
@@ -1,35 +1,11 @@
-/*#include <iostream>
-#include <fstream>
-
-class file{
-public:
-    file( std::string name = ("out.txt")) : fileName(name){
-        std::ifstream file(name);
-        if (!file.is_open()) // вызов метода is_open()
-            std::cout << "Not Working!" << std::endl;
-        else
-        {
-            std::cout << "Alles Gut!" << std::endl;
-        }
-    }
-private:
-    std::string fileName;
-};
-
-int main()
-{
-    //std::ifstream file_in ("out.txt");
-    file f;
-    return 0;
-}*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <map>
 
-std::map<std::string, std::map<std::string, std::string>> readIniFile(const std::string &filename) {
-    std::map<std::string, std::map<std::string, std::string>> iniData;
+template<typename KeyType, typename ValueType>
+std::map<KeyType, std::map<KeyType, ValueType>> readIniFile(const std::string &filename) {
+    std::map<KeyType, std::map<KeyType, ValueType>> iniData;
     std::ifstream file(filename);
 
     if (file.is_open()) {
@@ -52,7 +28,6 @@ std::map<std::string, std::map<std::string, std::string>> readIniFile(const std:
                     std::string key = line.substr(0, delimiterPos);
                     std::string value = line.substr(delimiterPos + 1);
 
-                    // Сохраняем данные в std::map
                     iniData[currentSection][key] = value;
                 }
             }
@@ -67,12 +42,11 @@ std::map<std::string, std::map<std::string, std::string>> readIniFile(const std:
 }
 
 int main() {
-    std::string filename = "D:\\ClionProjects\\Advanced_CPP_Programming\\INI-files_parser\\ini_file.ini";
-    std::map<std::string, std::map<std::string, std::string>> iniData = readIniFile(filename);
+    auto data = readIniFile<std::string, std::string>("D:\\ClionProjects\\Advanced_CPP_Programming\\INI-files_parser\\ini_file.ini");
 
-    for (const auto &section: iniData) {
+    for (const auto& section : data) {
         std::cout << "[" << section.first << "]" << std::endl;
-        for (const auto &pair: section.second) {
+        for (const auto& pair : section.second) {
             std::cout << pair.first << " = " << pair.second << std::endl;
         }
         std::cout << std::endl;
